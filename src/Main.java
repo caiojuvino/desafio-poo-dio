c607d3e5764c2baaf122830761df9e37657b5b99
Caio Fábio Juvino de Queiroz <caio.queiroz@ccc.ufcg.edu.br>
2023-03-26 22:19:23 -0300

    Finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso = new Curso("Curso Java", "Tudo sobre Java", 8);
+        Mentoria mentoria = new Mentoria("Mentoria Java","Mentoria de Java", LocalDate.now());
+
+        Bootcamp bootcamp = new Bootcamp("Java Developer", "Bootcamp para Java Developers");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev = new Dev("Caio");
+        dev.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos: " + dev.getConteudosInscritos());
+        dev.progredir();
+
+        System.out.println("Conteúdos Inscritos: " + dev.getConteudosInscritos());
+        System.out.println("XP Total: " + dev.calcularTotalXp());
     }
 }
--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,28 +2,20 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+public class Mentoria extends Conteudo{
 
-    private String titulo;
-    private String descricao;
     private LocalDate data;
 
-    public String getTitulo() {
-        return titulo;
+    public Mentoria(String titulo, String descricao, LocalDate data){
+        setTitulo(titulo);
+        setDescricao(descricao);
+        this.data = data;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20.0;
     }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
     public LocalDate getData() {
         return data;
     }
@@ -35,9 +27,10 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + data +
                 '}';
     }
+
 }
